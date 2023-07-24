869efb87cfcf7ad6baf0045bf87e88db4aeaa407
UdYnamite <89peterj89@gmail.com>
2023-07-24 14:16:13 +0700

    Added operation functions

---
+++ script.js
@@ -0,0 +1,46 @@
+//Calculator variables
+let firstNumber;
+let secondNumber;
+let operator;
+
+
+
+//Operate function. Take first and second number and operate accordingly.
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            multiply(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+///////////Operations/////////////
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+///////////////////////////////////

3def1b4f7c8772d7b4a7a80e9da802e27180d282
UdYnamite <89peterj89@gmail.com>
2023-07-26 15:05:57 +0700

    Added buttons handler Added keyboard bindings Allow number input and calculate simple equation

--- script.js
+++ script.js
@@ -1,28 +1,78 @@
+//Selectors
+const displayTop = document.querySelector('.small-display');
+const displayBottom = document.querySelector('.big-display');
+const numberButtons = document.querySelectorAll('.number-btn');
+const operatorButtons = document.querySelectorAll('.operator-btn');
+const allClearButton = document.querySelector('#all-clear');
+const equalButton = document.querySelector('#equal-btn');
+const deleteButton = document.querySelector('#delete-btn');
+const buttons = document.querySelectorAll('button');
 //Calculator variables
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let operator;
+let operated = false;
 //Display values
-let topValue;
-let bottomValue;
+let topValue = "0";
+let bottomValue = "";
 
 
+allClearButton.addEventListener('click', function() {
+    topValue = "0";
+    bottomValue = "";
+    displayTop.textContent = "0";
+    displayBottom.textContent = "";
+    firstNumber = "";
+    secondNumber = "";
+    operated = false
+});
+numberButtons.forEach(button => button.addEventListener('click', e => numberButtonHandler(e)));
+operatorButtons.forEach(button => button.addEventListener('click', e => operatorButtonHandler(e)));
+equalButton.addEventListener('click', equalButtonHandler);
+
+
+
+//Handle number buttons press
+function numberButtonHandler(e) {
+    //If zero clean display first
+    if (topValue === "0") {
+        topValue = "";
+    }
+    topValue += e.target.id;
+    displayTop.textContent = topValue;
+    if (operated) {
+        secondNumber += e.target.id;
+    } else {
+        firstNumber += e.target.id;
+    }
+}
+function operatorButtonHandler(e) {
+    operator = e.target.textContent;
+    operated = true;
+    topValue += ` ${e.target.textContent} `;
+    displayTop.textContent = topValue;
+}
+
+function equalButtonHandler() {
+    console.log("equal");
+    firstNumber = parseInt(firstNumber);
+    secondNumber = parseInt(secondNumber);
+    console.log(`${firstNumber}, ${secondNumber}, ${operator}`);
+    bottomValue = operate(firstNumber, secondNumber, operator);
+    displayBottom.textContent = bottomValue;
+}
 
 //Operate function. Take first and second number and operate accordingly.
 function operate(num1, num2, operator) {
     switch (operator) {
         case '+':
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case '-':
-            subtract(num1, num2);
-            break;
-        case '*':
-            multiply(num1, num2);
-            break;
-        case '/':
-            multiply(num1, num2);
-            break;
+            return subtract(num1, num2);
+        case '×':
+            return multiply(num1, num2);
+        case '÷':
+            return multiply(num1, num2);
         default:
             break;
     }
@@ -47,3 +97,35 @@ function divide(num1, num2) {
 }
 
 ///////////////////////////////////
+
+//Keybindings
+window.addEventListener('keydown', keyboardHandler);
+
+function keyboardHandler(e) {
+    console.log(e.key);
+    let clickedButton;
+    if (e.key == 'c') {
+        clickedButton = allClearButton;
+    } else if (e.key == 'Backspace' || e.key == 'Delete') {
+        clickedButton = deleteButton;
+    } else if (e.key == 'Enter' || e.key == '=') {
+        clickedButton = equalButton;
+    } else {
+        buttons.forEach(button => {
+            if (button.id == e.key) {
+                clickedButton = button;
+            }
+        });
+    }
+    if (clickedButton) {
+        clickedButton.click();
+        clickedButton.classList.add('active');
+    }
+}
+
+window.addEventListener('keyup', function(e) {
+    const buttonsActive = this.document.querySelectorAll('.active');
+    buttonsActive.forEach(function(button) {
+        button.classList.remove('active');
+    })
+})
